3b6520fb57f72a7142ef8f1918b96e2b0403faa2
JuniorLoch <72585178+JuniorLoch@users.noreply.github.com>
2022-04-01 20:03:02 -0400

    pequenos ajustes

--- JS/Formulário voto/serverform.js
+++ JS/Formulário voto/serverform.js
@@ -15,8 +15,11 @@ const server = http.createServer((req,res)=>{
             var idade = datanasc.diff(moment(),"years") * -1;
             console.log(idade);
             
-            if(idade < 16 && idade > 0){
-                //nao pode votar
+            if(idade <= 0){
+                res.writeHeader(200,{'Content-Type':'text/html'});
+                res.write('<head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> <title>Pode</title> </head> <body  class="#4dd0e1 cyan lighten-2"> <div class="container #b2ebf2 cyan lighten-4 center"> <div class="row col s12" style="padding-bottom: 30%;padding-top: 30%; margin-bottom: 0;"><h1> nem nasceu ainda ta querendo votar pq </h1><form action="index.html" method="post" enctype="multipart/form-data"><button class="btn btn-large #4dd0e1 cyan lighten-2" type="submit" name="action">Voltar</button></form></div></div></body>');
+            }else if(idade < 16 && idade > 0){
+                //nao pode vota
                 res.writeHeader(200,{'Content-Type':'text/html'});
                 res.write('<head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> <title>Pode</title> </head> <body  class="#4dd0e1 cyan lighten-2"> <div class="container #b2ebf2 cyan lighten-4 center"> <div class="row col s12" style="padding-bottom: 30%;padding-top: 30%; margin-bottom: 0;"><h1> Você nao pode votar nao </h1><form action="index.html" method="post" enctype="multipart/form-data"><button class="btn btn-large #4dd0e1 cyan lighten-2" type="submit" name="action">Voltar</button></form></div></div></body>');
             } else if (idade >= 16 && idade < 18){
@@ -25,7 +28,7 @@ const server = http.createServer((req,res)=>{
                 res.end();
             } else {
                 //deve votar
-                res.write('<head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> <title>Pode</title> </head> <body  class="#4dd0e1 cyan lighten-2"> <div class="container #b2ebf2 cyan lighten-4 center"> <div class="row col s12" style="padding-bottom: 30%;padding-top: 30%; margin-bottom: 0;"><h1> Você pode obrigatoriamente deve votar </h1><form action="index.html" method="post" enctype="multipart/form-data"><button class="btn btn-large #4dd0e1 cyan lighten-2" type="submit" name="action">Voltar</button></form></div></div></body>');
+                res.write('<head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> <title>Pode</title> </head> <body  class="#4dd0e1 cyan lighten-2"> <div class="container #b2ebf2 cyan lighten-4 center"> <div class="row col s12" style="padding-bottom: 30%;padding-top: 30%; margin-bottom: 0;"><h1> Você obrigatoriamente deve votar </h1><form action="index.html" method="post" enctype="multipart/form-data"><button class="btn btn-large #4dd0e1 cyan lighten-2" type="submit" name="action">Voltar</button></form></div></div></body>');
                 res.end();
             }
         });
